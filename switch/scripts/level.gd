2d780f34f6df7383956b9a67bf16d29b72614536
Adam Tong <adammh.tong04@gmail.com>
2024-10-12 20:06:04 +0100

    Created some images and started on main loop
    (got the button to link to level script)

--- switch/scripts/level.gd
+++ switch/scripts/level.gd
@@ -1,11 +1,23 @@
 extends Node2D
 
+var run = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	### load main screen with overlay (title, high score, user, player sprite)
+	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	# main loop
+	if (run == true):
+		
+		pass
 	pass
+
+
+func _on_play_button_pressed() -> void:
+	run = true
+	$"Play Button".visible = false

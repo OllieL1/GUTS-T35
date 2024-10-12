c97fa63cbfe01eb0631d22c4fb0f0ab968aecb69
Adam Tong <adammh.tong04@gmail.com>
2024-10-12 14:45:50 +0100

    First commit on HUD

--- switch/scripts/level.gd
+++ switch/scripts/level.gd
@@ -3,9 +3,17 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	##initially display the start menu
+	##have a starting animation
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	##wait for a user input to go off the start menu
+	
+	
+	##show high scores on main page
+	
+	
 	pass

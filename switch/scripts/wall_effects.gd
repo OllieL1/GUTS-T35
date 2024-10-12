5f077829a85ea450f137d20bd5c6f628447390de
Callum Cooper Lee <2758297C@student.gla.ac.uk>
2024-10-12 21:06:28 +0100

    cool effects

---
+++ switch/scripts/wall_effects.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				toggleWall()
+				
+func toggleWall():
+	var rightParticles = $"Right Wall Effects/RightParticles"
+	var leftParticles = $"Left Wall Effects/LeftParticles"
+	var rightGlow = $"Right Wall Effects/RightGlow"
+	var leftGlow = $"Left Wall Effects/LeftGlow"
+	leftParticles.emitting=!leftParticles.emitting
+	rightParticles.emitting=!rightParticles.emitting
+	leftGlow.visible=!leftGlow.visible
+	rightGlow.visible=!rightGlow.visible
